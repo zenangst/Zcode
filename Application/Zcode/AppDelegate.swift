662aa297994cb9cc425c50b9a40013a6fddf6c4f
Christoffer Winterkvist <chris@zenangst.com>
2015-07-01 21:05:27 +0200

    Push Zcode

--- Application/Zcode/AppDelegate.swift
+++ Application/Zcode/AppDelegate.swift
@@ -1,22 +1,13 @@
-# Xcode-Launcher
-<img src="https://raw.githubusercontent.com/zenangst/Xcode-Launcher/master/Images/icon_v3@2x.png" align="right">
-Zcode is a simple OS X application that helps you launch, activate and even open files from the Finder in Xcode 6.4 in El Capitan Developer Preview 2.
+//
+//  AppDelegate.swift
+//  Zcode
+//
+//  Created by Christoffer Winterkvist on 01/07/15.
+//  Copyright (c) 2015 zenangst. All rights reserved.
+//
 
-### How it works
+import Cocoa
 
-It's simple, it just checks if the Xcode process is running, if so; then it activates Xcode. However, if Xcode isn't running, then it launches Xcode via the terminal workaround. And if you chose to open files using Zcode, it will open it in Xcode 6.4.
-
-### Install
-
-Just download the latest release and drag and drop it into your applications folder, just like you would any other app. Then choose to always open Xcode related files with Zcode instead of Xcode.
-
-Or if you are feeling adventures, you could download the source code and tinker with it if you like.
-
-If you are looking for the old Apple Scripts, you can find them in the Scripts folder.
-
-### Source
-
-```swift
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
@@ -68,20 +59,4 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         task.launch()
     }
 }
-```
 
-### Contribute
-
-1. Fork it
-2. Create your feature branch (`git checkout -b my-new-feature`)
-3. Commit your changes (`git commit -am 'Add some feature'`)
-4. Push to the branch (`git push origin my-new-feature`)
-5. Create pull request
-
-### Who made this?
-
-- Christoffer Winterkvist ([@zenangst](https://twitter.com/zenangst))
-
-### Thanks
-
-A big shout-out to [Sash Zats](https://twitter.com/zats), who made the initial [Xcode-Launcher](https://twitter.com/zats/status/613464620997570560) on which this is based.
